56cb67f97ddfaeb4eadb48dc080e06680d2e3831
Karmo <Karmo.Kiima@Eesti.ee>
2020-04-16 17:20:26 +0300

    Update adding and displaying issues

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,10 +1,17 @@
 const Item = require('../models/item');
 
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset()); //-60
+    return local.toJSON().slice(0,16);
+});
+
 exports.getAdminPage = (req, res) => {
     Item.getItems((items)=>{
         res.render('admin', {
         pageTitle: 'Issues List',
         items: items,
+        timeNow: new Date().toDateInputValue(),
         path: '/admin'
         });
     });
--- models/item.js
+++ models/item.js
@@ -6,6 +6,22 @@ const dataPath = path.join(path.dirname(process.mainModule.filename),
     'item.json'        
     );
 
+Date.prototype.toDateInputValue = (function() {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset()); //-60
+    return local.toJSON().slice(0,16);
+});
+
+function sortByProperty(property){  
+    return function(a,b){  
+       if(a[property] > b[property])  
+          return 1;  
+       else if(a[property] < b[property])  
+          return -1;  
+       return 0;  
+    }  
+ }
+
 const getItemsFromFile = (callBack) => {
     fs.readFile(dataPath, (error, fileContent) => {
         if(error){
@@ -20,11 +36,14 @@ module.exports = class Item {
     constructor(itemDesc, itemResolveDate){
         this.itemDesc = itemDesc;
         this.itemResolveDate = itemResolveDate;
+        this.itemDateCreated = new Date().toDateInputValue();
     }
 
     saveItem() {
         getItemsFromFile(items => {
+            
             items.push(this);
+            items.sort(sortByProperty('itemResolveDate'));
             fs.writeFile(dataPath, JSON.stringify(items), (error) => {
                 console.log(error);
             });
